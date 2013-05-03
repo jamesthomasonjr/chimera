bddd075efda97cde38b469dc43d32720ccf8cea1
James Thomason <james.thomason4@gmail.com>
2013-05-03 12:55:04 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+/*
+ * Module dependencies
+ */
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+
+var app = express()
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Chimera' }
+  )
+})
+
+app.listen(3000);
+console.log("Now listening on port 3000");
